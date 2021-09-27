7874857dfd2394b5bbb4d23782d3d373fed3096c
“TorrLaura” <laplebe35@gmail.com>
2021-09-27 09:22:14 -0500

    completed DecisonMakingExercise

---
+++ script.js
@@ -0,0 +1,17 @@
+let randomNum = Math.floor(Math.random() * 5 +1);
+console.log(randomNum);
+
+let num=4
+if (num < 3){
+    console.log("num is less than 4");
+}else{
+    console.log("num is greater than or equal to 4");
+}
+
+let number = 3
+if(number ===2){
+    console.log("number is 2");
+}
+// else if(number ===4){
+//  console.log(number is 4);   
+// }
